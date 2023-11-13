a483e9337e82a1cd96caf06b951e4cfc85518e53
Michael Schernthaner <michaelschernthaner@MacBook-Pro-von-Michael.local>
2023-11-13 11:02:19 +0100

    final files

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,36 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route("/add")
+def getback_add():
+    """"Getting back an addition of a and b""" 
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    return str(result)
+
+@app.route("/subtract")
+def getback_subtract():
+    """"Getting back an addition of a and b""" 
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    return str(result)
+
+@app.route("/multiply")
+def getback_multiply():
+    """"Getting back an addition of a and b""" 
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+    return str(result)
+
+@app.route("/divide")
+def getback_divide():
+    """"Getting back an addition of a and b""" 
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    return str(result)
